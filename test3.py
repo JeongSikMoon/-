cd5355ca453c83e8310887a59829fe68837dbcc6
JeongSikMoon <50408357+JeongSikMoon@users.noreply.github.com>
2019-08-30 16:39:42 +0900

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 30 14:43:28 2019
+
+@author: JY-JS
+"""
+print('안녕')
+#%%
+print('hello')
+#%%
+print('파이썬은 재미있습니다.')
+#%%
+print('안녕' + '하세요')
+#%%
+print('안녕 ' * 5)
+#%%
+print(1)
+#%%
+print(1+1)
+#%%
+print(1-3)
+#%%
+print(2*3)
+#%%
+print(2**3)
+#%%
+print(5/2)
+#%%
+print('안녕','하세요')
+#%%
+print(5//2)
+#%%
+print(5%2)
+#%%
+print(True)
+#%%
+print(False)
+#%%
+print('1 + 4 = 5')
+#%%
+a = 3
+print(a)
+#%%
+c = True
+print(c)
+#%%
+bread = '김치'
+print(bread)
+#%%
+print('5'*5)
+#%%
+a = '5'
+print(a)
+b = '김치'
+milk = '물'
+_car = 153
+
+a= 33
+print(a)
+#%%
+1a = 1
+print(1a)
